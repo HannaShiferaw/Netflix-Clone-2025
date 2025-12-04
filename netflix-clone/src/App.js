2e88aec9ee91ec2458d30ddc470fa7adc343e218
Hanna Shiferaw <hannashiferaw13@gmail.com>
2025-12-04 13:04:25 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import './index.css'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+     <Home />
+
+
     </div>
   );
 }
