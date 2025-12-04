a6ba2a34488c07947b812d57d3e1fa624e9ea16e
Hanna Shiferaw <hannashiferaw13@gmail.com>
2025-12-04 17:43:43 +0300

    row updated

--- netflix-clone/src/Components/Rows/RowList/RowList.js
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -5,7 +5,7 @@ import requests from '../../../utils/requests'
 const RowList = () => {
   return (
     <>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
